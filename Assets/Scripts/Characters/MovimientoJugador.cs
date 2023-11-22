852a8835c0fbb34d4ecb80c65deff21e97286da8
DESKTOP-BGDQUAH\sasuk <fernandozam2305@outlook.com>
2023-11-22 13:33:39 -0600

    ya casi mi niño mi pa

--- Assets/Scripts/Characters/MovimientoJugador.cs
+++ Assets/Scripts/Characters/MovimientoJugador.cs
@@ -7,42 +7,36 @@ public class MovimientoJugador : MonoBehaviour
     private Rigidbody2D rb2D;
 
     [Header("Movimiento")]
-    private float inputX;
-
     private float movimientoHorizontal = 0f;
-
-    [SerializeField] public float velocidadDeMovimiento;
-    [SerializeField] GameObject player;
-
-    [Range(0f, 0.3f)] [SerializeField] private float suavizadoDeMovimiento;
-
     private Vector3 velocidad = Vector3.zero;
-
     private bool mirandoDerecha = true;
+
+    [SerializeField] private float velocidadDeMovimiento;
+    [Range(0, 0.3f)] [SerializeField] private float suavizadoDeMovimiento;
 
     [SerializeField] private float fuerzaDeSalto;
     [SerializeField] private LayerMask queEsSuelo;
     [SerializeField] private Transform controladorSuelo;
     [SerializeField] private Vector3 dimensionesCaja;
     [SerializeField] private bool enSuelo;
-
-    [Header("SaltoPared")]
-    [SerializeField] private Transform controladorPared;
-    [SerializeField] private Vector3 dimensionesCajaPared;
-    private bool enPared;
-    private bool deslizando;
-    [SerializeField] private float velocidadDeslizar;
-
-    [SerializeField] private float fuerzaSaltoParedX;
-    [SerializeField] private float fuerzaSaltoParedY;
-    [SerializeField] private float tiempoSaltoPared;
-
-    public bool saltandoDePared;
     private bool salto = false;
 
-    [Header("Animacion")]
-
     private Animator animator;
+
+    private float inputX;
+
+    [SerializeField] private Transform controladorPared;
+    [SerializeField] private Vector3 dimensionesCajaPared;
+    [SerializeField] private float velocidadDeslizar;
+    private bool enPared;
+    private bool deslizando;
+
+    [SerializeField] private float fuerzaSaltoX;
+    [SerializeField] private float fuerzaSaltoY;
+    [SerializeField] private float tiempoSaltoPared;
+    private bool saltandoDePared;
+
+
 
     private void Start()
     {
@@ -52,7 +46,6 @@ public class MovimientoJugador : MonoBehaviour
 
     private void Update()
     {
-        rb2D.velocity = new Vector2(velocidadDeMovimiento, rb2D.velocity.y);
         inputX = Input.GetAxisRaw("Horizontal");
         movimientoHorizontal = inputX * velocidadDeMovimiento;
         animator.SetFloat("Horizontal", Mathf.Abs(movimientoHorizontal));
@@ -62,22 +55,22 @@ public class MovimientoJugador : MonoBehaviour
             salto = true;
         }
 
-        if (!enSuelo && enPared && inputX != 0)
+        if(!enSuelo && enPared && inputX != 0)
         {
             deslizando = true;
         }
         else
         {
-            deslizando = false;
+            deslizando = false; 
         }
     }
-
     private void FixedUpdate()
     {
         enSuelo = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCaja, 0f, queEsSuelo);
         animator.SetBool("enSuelo", enSuelo);
         Mover(movimientoHorizontal * Time.fixedDeltaTime, salto);
         enPared = Physics2D.OverlapBox(controladorPared.position, dimensionesCajaPared, 0f, queEsSuelo);
+
         salto = false;
 
         if (deslizando)
@@ -94,32 +87,31 @@ public class MovimientoJugador : MonoBehaviour
             rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, velocidadObjetivo, ref velocidad, suavizadoDeMovimiento);
         }
 
-        if (mover > 0f && !mirandoDerecha)
-        {
-            Girar();
-        }
-        else if (mover < 0f && mirandoDerecha)
+        if(mover > 0 && !mirandoDerecha)
         {
             Girar();
         }
 
-        if (saltar && enSuelo && !deslizando)
+        else if(mover < 0 && mirandoDerecha)
         {
-            enSuelo = false;
-            rb2D.AddForce(new Vector2(0f, fuerzaDeSalto));
+            Girar();
+        }
+
+        if(saltar && enSuelo && !deslizando)
+        {
+            Salto();
         }
 
         if (saltar && enPared && deslizando)
         {
-            saltoPared();
+            SaltoPared();
         }
-
     }
 
-    private void saltoPared()
+    private void SaltoPared()
     {
         enPared = false;
-        rb2D.velocity = new Vector2(fuerzaSaltoParedY * -inputX, fuerzaSaltoParedY);
+        rb2D.velocity = new Vector2(fuerzaSaltoX * -inputX, fuerzaSaltoY);
         StartCoroutine(CambioSaltoPared());
     }
 
@@ -130,19 +122,26 @@ public class MovimientoJugador : MonoBehaviour
         saltandoDePared = false;
     }
 
+    private void Salto()
+    {
+        enSuelo = false;
+        rb2D.AddForce(new Vector2(0f, fuerzaDeSalto));
+    }
+
     private void Girar()
     {
         mirandoDerecha = !mirandoDerecha;
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180);
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
+        Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(controladorSuelo.position, dimensionesCaja);
         Gizmos.DrawWireCube(controladorPared.position, dimensionesCajaPared);
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "CaidaVacio")
